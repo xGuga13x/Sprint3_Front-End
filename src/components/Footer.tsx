c632e785d455bdc75a5373d0288f7b2a08e63514
Gustavo Jesus Silva <RM567926@fiap.com.br>
2026-04-10 20:49:14 -0300

    Cria Footer
    Cria componente Footer reutilizável

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gradient-to-r from-[#eddd53] via-[#57c785] to-[#004857] text-white text-center py-6 px-6 text-sm">
+      <p>
+        &copy; 2025 De Novo Não! | Todos os direitos reservados. | Projeto
+        desenvolvido em parceria com a Turma do Bem.
+      </p>
+    </footer>
+  )
+}
